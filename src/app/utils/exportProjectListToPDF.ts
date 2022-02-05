dd5a09ae1bf3c0a2f5f5cbabd63c765df34329cb
Piotr Ferensztajn <piotrferensztajn@Piotrs-MacBook-Pro.local>
2022-02-05 21:29:27 +0100

    Finalized fn for exporting project list to pdf

--- src/app/utils/exportProjectListToPDF.ts
+++ src/app/utils/exportProjectListToPDF.ts
@@ -2,53 +2,57 @@ import { jsPDF } from 'jspdf';
 import { getClosestPastMonday } from './index';
 
 export const exportProjectListToPDF = (elem: HTMLElement): void => {
-  console.log(elem);
+  const listNode: Node = elem.cloneNode(true);
+  const list = document.createElement('div');
+  list.appendChild(listNode);
+
+  const icons = list.querySelectorAll('.bi-envelope');
+  const buttons = list.querySelectorAll('.btn-primary--light');
+  const alerts = list.querySelectorAll(".alert");
+
+  icons.forEach((icon) => {
+    icon.parentElement?.removeChild(icon);
+  });
+
+  alerts.forEach(alert => {
+    alert.parentElement?.removeChild(alert);
+  })
+
+  buttons.forEach((button) => {
+    button.classList.remove('btn-primary--light');
+    button.classList.add('btn-primary--light-printable');
+  });
+
   const doc = new jsPDF({
     orientation: 'landscape',
     format: 'a4',
-    unit: 'pt',
+    unit: 'px',
   });
-  const date = getClosestPastMonday(new Date());
-
+  const listHeader: HTMLElement | null = elem.querySelector('.entry-header');
   const projectList: HTMLElement | null =
     elem.querySelector('.project-content');
 
-  if (!projectList) {
+  if (!projectList || !listHeader) {
     return;
   }
 
-  const canvasHeight = projectList.scrollHeight + 100;
+  const date = getClosestPastMonday(new Date());
+  const elemWidth = projectList.scrollWidth;
+  const elemHeight = projectList.scrollHeight;
+  const pageWidth = doc.internal.pageSize.getWidth();
 
-  // 541
-  // 942
-
-  const width = doc.internal.pageSize.getWidth();
-  const height = doc.internal.pageSize.getHeight();
-
-  console.log({
-    width,
-  });
-
-  doc.html(elem, {
+  doc.html(list, {
     callback: function (doc) {
       doc.save();
     },
-    margin: 5,
+    html2canvas: {
+      windowHeight: elemHeight * 2,
+      scrollY: -window.scrollY,
+    },
+    margin: [15, 0, 15, 0],
     autoPaging: 'text',
     filename: `Client dev ${date.toLocaleDateString()}`,
-
-    html2canvas: {
-      windowHeight: canvasHeight * 2,
-    },
-    // x: 5,
-    // y: 5,
+    windowWidth: elemWidth,
+    width: pageWidth,
   });
-};
-
-const pixToPoints = (pix: number): number => {
-  return pix + pix * 0.25;
-};
-
-const pointsToPix = (points: number): number => {
-  return points * 0.5;
 };
